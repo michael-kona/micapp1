bcc3223ac1e2a1894eb97c663d74e186f4e0ba72
michael-kona <michael.kona@gmail.com>
2022-08-05 14:20:54 +0530

    Initial Commit

--- app/src/main/java/com/example/micapp1/DisplayNameActivity.java
+++ app/src/main/java/com/example/micapp1/DisplayNameActivity.java
@@ -24,7 +24,7 @@ public class DisplayNameActivity extends AppCompatActivity
         //prepare layout for toast
         LayoutInflater li=getLayoutInflater();
         View toast_layout=li.inflate(R.layout.custom_toast_layout,(ViewGroup)findViewById(R.id.custom_toast_root));
-        TextView toast_txt_view=(TextView)findViewById(R.id.textView2);
+        TextView toast_txt_view=(TextView)toast_layout.findViewById(R.id.textView4);
         toast_txt_view.setText(message);
         //custom toast
         Toast t=new Toast(this.getApplicationContext());
